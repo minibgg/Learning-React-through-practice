765ceba95251f814a2e7cbb3ebcd895ee19cc287
minibgg <ivangey984@gmail.com>
2026-08-11 04:20:18 +0300

    feat: complete React lesson 3

--- react-lesson-2/src/App.jsx
+++ react-lesson-2/src/App.jsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react";
 
+const players = [
+  { id: 1, name: "John", level: 25, winrate: 57 },
+  { id: 2, name: "Ivan", level: 18, winrate: 49 },
+  { id: 3, name: "Alex", level: 32, winrate: 63 },
+  { id: 4, name: "Mike", level: 12, winrate: 54 },
+  { id: 5, name: "Daniel", level: 27, winrate: 46 },
+];
+
 export default function App() {
   const [users, setUsers] = useState([]);
   const [filterInput, setFilterInput] = useState("");
+  useEffect(() => {
+    setUsers(players);
+  }, []);
 
   function FilterInput(props) {
     return (
@@ -17,26 +28,51 @@ export default function App() {
   }
 
   function User(props) {
-    return <div>{props.user.name}</div>;
+    return (
+      <>
+        <div>
+          ID: {props.user.id} | Name: {props.user.name} | level:{" "}
+          {props.user.level} | winrate: {props.user.winrate} |{" "}
+        </div>
+      </>
+    );
+  }
+
+  function ButtonLvlUp(props) {
+    function lvlUp(id) {
+      const newUsers = props.users.map((user) => {
+        if (user.id === id) {
+          return {
+            ...user,
+            level: user.level + 1,
+          };
+        }
+
+        return user;
+      });
+
+      props.setUsers(newUsers);
+    }
+
+    return <button onClick={() => lvlUp(props.user.id)}>+</button>;
   }
 
   function UserList(props) {
     return (
       <>
         {props.users.map((user) => (
-          <User key={user.id} user={user} />
+          <div key={user.id}>
+            <User user={user} />
+            <ButtonLvlUp
+              user={user}
+              users={props.allUsers}
+              setUsers={props.setUsers}
+            />
+          </div>
         ))}
       </>
     );
   }
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data);
-      });
-  }, []);
 
   const filteredUsers = users.filter(function (user) {
     if (filterInput === "") {
@@ -52,7 +88,11 @@ export default function App() {
         setInput={setFilterInput}
         placeholder="Search user"
       />
-      <UserList users={filteredUsers}></UserList>
+      <UserList
+        users={filteredUsers}
+        allUsers={users}
+        setUsers={setUsers}
+      ></UserList>
     </>
   );
 }
