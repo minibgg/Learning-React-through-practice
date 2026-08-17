2ff2e2d4ba529df22a3d7635f518a8e787befb32
minibgg <ivangey984@gmail.com>
2026-08-17 04:00:41 +0300

    feat: complete lesson 5 repeat useMemo useCallBack and more

---
+++ react-lesson-5/src/App.jsx
@@ -0,0 +1,121 @@
+import { useCallback, useState, useMemo } from "react";
+
+const players = [
+  { id: 1, name: "John", level: 25, winrate: 57 },
+  { id: 2, name: "Ivan", level: 18, winrate: 49 },
+  { id: 3, name: "Alex", level: 32, winrate: 63 },
+  { id: 4, name: "Mike", level: 12, winrate: 54 },
+  { id: 5, name: "Daniel", level: 27, winrate: 46 },
+];
+
+function HighestLevel(props) {
+  const copyPlayers = [...props.users];
+  const highestPlayer = copyPlayers.sort((a, b) => b.level - a.level)[0];
+
+  return <>Highest level: {highestPlayer.level}</>;
+}
+
+function ResetLevel(props) {
+  function resetLvl() {
+    props.setUsers(players);
+  }
+
+  return <button onClick={() => resetLvl()}>RESET LEVELS</button>;
+}
+
+function AverageWinrate(props) {
+  const totalWinrate = props.users.reduce((total, user) => {
+    return total + user.winrate;
+  }, 0);
+  const averageWinrate = totalWinrate / props.users.length;
+
+  return <>Average winrate: {averageWinrate}</>;
+}
+
+function User(props) {
+  return (
+    <span>
+      name: {props.user.name} | level: {props.user.level} | winrate:{" "}
+      {props.user.winrate}
+    </span>
+  );
+}
+
+function LvlUpButton(props) {
+  return <button onClick={() => props.lvlUp(props.user.id)}>+</button>;
+}
+
+function UserList(props) {
+  return props.users.map((user) => {
+    return (
+      <div key={user.id}>
+        <User user={user} />
+        <LvlUpButton
+          users={props.users}
+          setUsers={props.setUsers}
+          user={user}
+          lvlUp={props.lvlUp}
+        />
+      </div>
+    );
+  });
+}
+
+function FilterInput(props) {
+  return (
+    <input
+      onChange={(e) => props.onChange(e.target.value)}
+      value={props.value}
+      placeholder={props.placeholder}
+    />
+  );
+}
+
+function PlayerStats(props) {
+  return <>сейчас {props.users.length} игроков</>;
+}
+
+export default function App() {
+  const [users, setUsers] = useState(players);
+  const [filterInput, setFilterInput] = useState("");
+
+  const handleLvlUp = useCallback((id) => {
+    setUsers((prevUsers) => {
+      return prevUsers.map((user) => {
+        if (user.id === id) {
+          return {
+            ...user,
+            level: user.level + 1,
+          };
+        }
+
+        return user;
+      });
+    });
+  }, []);
+  const filteredUsers = useMemo(() => {
+    users.filter(function (user) {
+      if (filterInput === "") {
+        return true;
+      } else {
+        return user.name.toLowerCase().includes(filterInput.toLowerCase());
+      }
+    });
+  }, [users, filterInput]);
+
+  return (
+    <>
+      <FilterInput
+        value={filterInput}
+        onChange={setFilterInput}
+        placeholder="Search for name"
+      />
+      <UserList lvlUp={handleLvlUp} setUsers={setUsers} users={filteredUsers} />
+      <PlayerStats users={users} />
+      <ResetLevel setUsers={setUsers} />
+      <AverageWinrate users={users} />
+      <br />
+      <HighestLevel users={users} />
+    </>
+  );
+}
