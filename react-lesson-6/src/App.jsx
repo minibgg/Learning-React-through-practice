d96e90da04a13e8de1bd97699f53e322a125fa9e
minibgg <ivangey984@gmail.com>
2026-08-17 13:22:16 +0300

    feat: add base for lesson 6

--- react-lesson-6/src/App.jsx
+++ react-lesson-6/src/App.jsx
@@ -1,122 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useCallback, useState, useMemo } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const players = [
+  { id: 1, name: "John", level: 25, winrate: 57 },
+  { id: 2, name: "Ivan", level: 18, winrate: 49 },
+  { id: 3, name: "Alex", level: 32, winrate: 63 },
+  { id: 4, name: "Mike", level: 12, winrate: 54 },
+  { id: 5, name: "Daniel", level: 27, winrate: 46 },
+];
+
+function HighestLevel(props) {
+  const copyPlayers = [...props.users];
+  const highestPlayer = copyPlayers.sort((a, b) => b.level - a.level)[0];
+
+  return <>Highest level: {highestPlayer.level}</>;
+}
+
+function ResetLevel(props) {
+  function resetLvl() {
+    props.setUsers(players);
+  }
+
+  return <button onClick={() => resetLvl()}>RESET LEVELS</button>;
+}
+
+function AverageWinrate(props) {
+  const totalWinrate = props.users.reduce((total, user) => {
+    return total + user.winrate;
+  }, 0);
+  const averageWinrate = totalWinrate / props.users.length;
+
+  return <>Average winrate: {averageWinrate}</>;
+}
+
+function User(props) {
+  return (
+    <span>
+      name: {props.user.name} | level: {props.user.level} | winrate:{" "}
+      {props.user.winrate}
+    </span>
+  );
+}
+
+function LvlUpButton(props) {
+  return <button onClick={() => props.lvlUp(props.user.id)}>+</button>;
+}
+
+function UserList(props) {
+  return props.users.map((user) => {
+    return (
+      <div key={user.id}>
+        <User user={user} />
+        <LvlUpButton
+          users={props.users}
+          setUsers={props.setUsers}
+          user={user}
+          lvlUp={props.lvlUp}
+        />
+      </div>
+    );
+  });
+}
+
+function FilterInput(props) {
+  return (
+    <input
+      onChange={(e) => props.onChange(e.target.value)}
+      value={props.value}
+      placeholder={props.placeholder}
+    />
+  );
+}
+
+function PlayerStats(props) {
+  return <>сейчас {props.users.length} игроков</>;
+}
+
+export default function App() {
+  const [users, setUsers] = useState(players);
+  const [filterInput, setFilterInput] = useState("");
+
+  const handleLvlUp = useCallback((id) => {
+    setUsers((prevUsers) => {
+      return prevUsers.map((user) => {
+        if (user.id === id) {
+          return {
+            ...user,
+            level: user.level + 1,
+          };
+        }
+
+        return user;
+      });
+    });
+  }, []);
+  const filteredUsers = useMemo(() => {
+    users.filter(function (user) {
+      if (filterInput === "") {
+        return true;
+      } else {
+        return user.name.toLowerCase().includes(filterInput.toLowerCase());
+      }
+    });
+  }, [users, filterInput]);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <FilterInput
+        value={filterInput}
+        onChange={setFilterInput}
+        placeholder="Search for name"
+      />
+      <UserList lvlUp={handleLvlUp} setUsers={setUsers} users={filteredUsers} />
+      <PlayerStats users={users} />
+      <ResetLevel setUsers={setUsers} />
+      <AverageWinrate users={users} />
+      <br />
+      <HighestLevel users={users} />
     </>
-  )
+  );
 }
-
-export default App
